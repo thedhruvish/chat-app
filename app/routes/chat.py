9df4cfc004f850e6d07d9a8e23be92f93c2eea86
thedhruvish <thedhruvish@gmail.com>
2025-03-11 10:26:50 +0530

    add ruff

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -12,42 +12,42 @@ templates = Jinja2Templates(directory="app/templates")
 
 @route.get("/", response_class=HTMLResponse)
 async def get(req: Request):
-    return templates.TemplateResponse("index.html", {"request": req})
+	return templates.TemplateResponse("index.html", {"request": req})
 
 
 @route.websocket("/")
 async def websocket_endpoint(websocket: WebSocket):
-    client_id = random.randint(1000, 9999)
-    await manager.connect(websocket, client_id)
+	client_id = random.randint(1000, 9999)
+	await manager.connect(websocket, client_id)
 
-    # Send client ID to the connected user
-    await manager.send_personal_message(
-        json.dumps({"type": "init", "client_id": client_id}), websocket
-    )
+	# Send client ID to the connected user
+	await manager.send_personal_message(
+		json.dumps({"type": "init", "client_id": client_id}), websocket
+	)
 
-    try:
-        while True:
-            raw_data = await websocket.receive_text()
-            try:
-                data = json.loads(raw_data)
-                message = data.get("message", "")
-            except json.JSONDecodeError:
-                message = raw_data
+	try:
+		while True:
+			raw_data = await websocket.receive_text()
+			try:
+				data = json.loads(raw_data)
+				message = data.get("message", "")
+			except json.JSONDecodeError:
+				message = raw_data
 
-            response = {
-                "type": "message",
-                "message": message,
-                "client_id": client_id,
-                "img_like": f"https://api.dicebear.com/7.x/personas/svg?seed={client_id}",
-            }
+			response = {
+				"type": "message",
+				"message": message,
+				"client_id": client_id,
+				"img_like": f"https://api.dicebear.com/7.x/personas/svg?seed={client_id}",
+			}
 
-            await manager.broadcast(json.dumps(response), exclude=websocket)
+			await manager.broadcast(json.dumps(response), exclude=websocket)
 
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(
-            json.dumps(
-                {"type": "message", "message": f"Client #{client_id} left the chat"}
-            )
-        )
-        await manager.broadcast_user_count()
+	except WebSocketDisconnect:
+		manager.disconnect(websocket)
+		await manager.broadcast(
+			json.dumps(
+				{"type": "message", "message": f"Client #{client_id} left the chat"}
+			)
+		)
+		await manager.broadcast_user_count()
--- app/utile/chat_manage.py
+++ app/utile/chat_manage.py
@@ -3,36 +3,36 @@ import json
 
 
 class ConnectionManager:
-    def __init__(self):
-        self.active_connections: dict[WebSocket, int] = {}
+	def __init__(self):
+		self.active_connections: dict[WebSocket, int] = {}
 
-    async def connect(self, websocket: WebSocket, client_id: int):
-        await websocket.accept()
-        self.active_connections[websocket] = client_id
-        await self.broadcast_user_count()
+	async def connect(self, websocket: WebSocket, client_id: int):
+		await websocket.accept()
+		self.active_connections[websocket] = client_id
+		await self.broadcast_user_count()
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.pop(websocket, None)
+	def disconnect(self, websocket: WebSocket):
+		self.active_connections.pop(websocket, None)
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+	async def send_personal_message(self, message: str, websocket: WebSocket):
+		await websocket.send_text(message)
 
-    async def broadcast(self, message: str, exclude: WebSocket = None):  # type: ignore
-        for connection in self.active_connections:
-            if connection != exclude:
-                await connection.send_text(message)
+	async def broadcast(self, message: str, exclude: WebSocket = None):  # type: ignore
+		for connection in self.active_connections:
+			if connection != exclude:
+				await connection.send_text(message)
 
-    async def broadcast_user_count(self):
-        count_message = json.dumps(
-            {
-                "type": "user_count",
-                "count": len(self.active_connections),
-                "dp_img": "https://fastapi.tiangolo.com/img/favicon.png",
-                "user_name": "FastAPI",
-            }
-        )
-        for connection in self.active_connections:
-            await connection.send_text(count_message)
+	async def broadcast_user_count(self):
+		count_message = json.dumps(
+			{
+				"type": "user_count",
+				"count": len(self.active_connections),
+				"dp_img": "https://fastapi.tiangolo.com/img/favicon.png",
+				"user_name": "FastAPI",
+			}
+		)
+		for connection in self.active_connections:
+			await connection.send_text(count_message)
 
 
 manager = ConnectionManager()
